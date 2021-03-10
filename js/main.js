b239388d4fb2a80aee612cb3a677db2bf0f70ae2
Addressday <58471834+Addressday@users.noreply.github.com>
2021-03-10 17:06:27 +0900

    publish

--- js/main.js
+++ js/main.js
@@ -177,15 +177,15 @@
 
 // tab
 const btns = document.querySelectorAll(".tabs__button");
-const tabContent = document.querySelectorAll(".tab-content");
+const tabContent = document.querySelectorAll(".tab__content");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", () => {
     addClassFunc(btns[i], "tabs__button--active");
     clearClassFunc(i, btns, "tabs__button--active");
 
-    addClassFunc(tabContent[i], "tab-content--active");
-    clearClassFunc(i, tabContent, "tab-content--active");
+    addClassFunc(tabContent[i], "tab__content--active");
+    clearClassFunc(i, tabContent, "tab__content--active");
   });
 }
 
@@ -204,68 +204,3 @@ function clearClassFunc(indx, elems, elemClass) {
 
 
 
-// img
-let currentPage = 1;
-
-const DATA_PER_PAGE = 10,
-  lastPage = 5;
-
-const msgLoading = document.getElementById("msg-loading");
-
-// 데이터 추가 함수
-function addData(currentPage) {
-  const $list = document.getElementById("list");
-
-  for (
-    let i = (currentPage - 1) * DATA_PER_PAGE + 1;
-    i <= currentPage * DATA_PER_PAGE;
-    i++
-  ) {
-    const $li = document.createElement("li");
-    $li.innerHTML = "이미지<img src='/img/sub/sub_1'>"
-    $li.classList.add("fade-in");
-    $list.appendChild($li);
-  }
-}
-
-// IntersectionObserver 갱신 함수
-function observeLastChild(intersectionObserver) {
-  const listChildren = document.querySelectorAll("#list li");
-  listChildren.forEach((el) => {
-    if (!el.nextSibling && currentPage < lastPage) {
-      intersectionObserver.observe(el); // el에 대하여 관측 시작
-    } else if (currentPage >= lastPage) {
-      intersectionObserver.disconnect();
-      msgLoading.textContent = "페이지의 끝입니다.";
-    }
-  });
-}
-
-// IntersectionObeserver 부분
-const observerOption = {
-  root: null,
-  rootMargin: "0px 0px 0px 0px",
-  threshold: 0.5
-};
-
-// IntersectionObserver 인스턴스 생성
-const io = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    // entry.isIntersecting: 특정 요소가 뷰포트와 50%(threshold 0.5) 교차되었으면
-    if (entry.isIntersecting) {
-      msgLoading.classList.add("fade-in");
-      // 다음 데이터 가져오기: 자연스러운 연출을 위해 setTimeout 사용
-      setTimeout(() => {
-        addData(++currentPage);
-        observer.unobserve(entry.target);
-        observeLastChild(observer);
-
-        msgLoading.classList.remove("fade-in");
-      }, 1000);
-    }
-  });
-}, observerOption);
-
-// 초기 데이터 생성
-addData(currentPage);
-observeLastChild(io);
